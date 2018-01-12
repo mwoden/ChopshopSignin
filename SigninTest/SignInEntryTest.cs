dbf6fcfc8bfd3f68bbf88bff574e4a2f72e54bd9
Mark Oden <mwoden@gmail.com>
2018-01-11 20:48:58 -0500

    Add more test support

---
+++ SigninTest/SignInEntryTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SigninTest
+{
+    [TestClass]
+    class SignInEntryTest
+    {
+        static readonly DateTime entryTime = new DateTime(2017, 12, 1, 18, 0, 0);
+        static readonly DateTime exitTime = new DateTime(2017, 12, 1, 21, 0, 0);
+
+        [TestMethod]
+        public void CreateCompleteSignInEntryTest()
+        {
+            var signIn = new SignInLibrary.SignInEntry(entryTime, exitTime);
+
+            Assert.AreEqual(signIn.IsOut, true);
+            Assert.AreEqual(signIn.IsIn, false);
+
+            Assert.AreEqual(false, true);
+        }
+    }
+}
